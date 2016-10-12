9690ec60295d4c8e65735e4ff1c8eca953dd7395
Andreas Grapentin <andreas@grapentin.org>
2016-10-12 12:54:13 +0200

    fixing lint errors

--- pyspaces/__init__.py
+++ pyspaces/__init__.py
@@ -1,59 +1,104 @@
+'''
+This module implements a simple client and server component for tuple spaces.
+'''
 
 from xmlrpc.client import ServerProxy
 from threading import Lock, Condition
 
 
 class PySpace(object):
+    '''
+    The tuple spaces client.
+    '''
 
     def __init__(self, server):
+        '''
+        constructor - connect to the given server.
+        '''
         self._server = server
         self._proxy = ServerProxy(server, allow_none=True, use_builtin_types=True)
 
-    def put(self, t):
-        if not isinstance(t, tuple):
+    def put(self, tpl):
+        '''
+        put the given tuple into the tuple space.
+        This method is a proxy for the server side method.
+        '''
+        if not isinstance(tpl, tuple):
             raise ValueError('parameter needs to be a tuple')
-        return self._proxy.put(t)
+        return self._proxy.put(tpl)
 
-    def take(self, t):
-        if not isinstance(t, tuple):
+    def take(self, tpl):
+        '''
+        take the queried tuple from the tuple space and return it.
+        This method is a proxy for the server side method.
+        '''
+        if not isinstance(tpl, tuple):
             raise ValueError('parameter needs to be a tuple')
-        return tuple(self._proxy.take(t))
+        return tuple(self._proxy.take(tpl))
 
-    def peek(self, t):
-        if not isinstance(t, tuple):
+    def peek(self, tpl):
+        '''
+        seek the given tuple in the tuple space and return it.
+        This method is a proxy for the server side method.
+        '''
+        if not isinstance(tpl, tuple):
             raise ValueError('parameter needs to be a tuple')
-        return tuple(self._proxy.peek(t))
+        return tuple(self._proxy.peek(tpl))
 
 
 class PySpaceServer(object):
+    '''
+    this class implements the tuple space api on the server side.
+    '''
 
     def __init__(self):
+        '''
+        constructor - prepare the tuple storage and the synchronization
+        primitives.
+        '''
         self._tuples = []
 
         self._lock = Lock()
         self._condition = Condition()
 
-    def put(self, t):
+    def put(self, tpl):
+        '''
+        put the given tuple into the tuple space.
+        '''
         with self._condition:
-            self._tuples.append(tuple(t))
+            self._tuples.append(tuple(tpl))
             self._condition.notify_all()
 
-    def take(self, t):
+    def take(self, tpl):
+        '''
+        take the queried tuple from the tuple space and return it.
+        '''
         while True:
             with self._condition:
-                r = self._find(t)
-                if r is not None:
-                    self._tuples.remove(r)
-                    return r
+                res = self._find(tpl)
+                if res is not None:
+                    self._tuples.remove(res)
+                    return res
                 self._condition.wait()
 
-    def peek(self, t):
+    def peek(self, tpl):
+        '''
+        seek the given tuple in the tuple space and return it.
+        '''
         while True:
             with self._condition:
-                r = self._find(t)
-                if r is not None:
-                    return r
+                res = self._find(tpl)
+                if res is not None:
+                    return res
                 self._condition.wait()
 
-    def _find(self, t):
-        return next((r for r in self._tuples if len(r) == len(t) and all(x == y or y is None for (x, y) in zip(r, t))), None)
+    def _find(self, tpl):
+        '''
+        find the queried tuple in the tuple space and return it, or None if
+        not found.
+        '''
+        return next((
+            res
+            for res in self._tuples
+            if len(res) == len(tpl) and all(x == y or y is None for (x, y) in zip(res, tpl))
+        ), None)
--- pyspaces/__main__.py
+++ pyspaces/__main__.py
@@ -1,3 +1,6 @@
+'''
+this module is the entry point for the tuple spaces server.
+'''
 
 from socketserver import ThreadingMixIn
 from xmlrpc.server import SimpleXMLRPCServer
@@ -5,9 +8,21 @@ from xmlrpc.server import SimpleXMLRPCServer
 from pyspaces import PySpaceServer
 
 
-class ThreadedXMLRPCServer(ThreadingMixIn,SimpleXMLRPCServer): pass
+class ThreadedXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
+    '''
+    this mixin produces a threaded version of SimpleXMLRPCServer used below.
+    '''
+    pass
 
-if __name__ == '__main__':
+
+def main():
+    '''
+    entry point - invoked when run from the command line.
+    '''
     server = ThreadedXMLRPCServer(('localhost', 10000), allow_none=True, use_builtin_types=True)
     server.register_instance(PySpaceServer())
     server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
